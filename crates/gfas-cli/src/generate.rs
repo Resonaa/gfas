31015018cc9c1f29686cef2b5e68d04cf63f9c4b
Resonaa <resonaa@outlook.com>
2024-08-31 22:20:13 +0800

    feat(cli): generate shell completions

--- crates/gfas-cli/src/generate.rs
+++ crates/gfas-cli/src/generate.rs
@@ -1,6 +1,7 @@
 use std::io;
 
-use clap::{CommandFactory, ValueEnum};
+use clap::{Command, CommandFactory, ValueEnum};
+use clap_complete::aot::{generate as generate_completions, Generator, Shell};
 
 use crate::cli::Cli;
 
@@ -11,25 +12,51 @@ pub enum GenerateMode {
     Man,
 
     /// Completions for bash
-    CompleteBash,
+    Bash,
 
     /// Completions for zsh
-    CompleteZsh,
+    Zsh,
 
     /// Completions for fish
-    CompleteFish,
+    Fish,
+
+    /// Completions for Elvish
+    Elvish,
 
     /// Completions for PowerShell
-    CompletePowerShell
+    #[value(name = "ps")]
+    PowerShell
+}
+
+fn complete<G: Generator>(gen: G, mut cmd: Command) {
+    let bin_name = cmd.get_name().to_string();
+    generate_completions(gen, &mut cmd, bin_name, &mut io::stdout());
 }
 
 /// Generates man page or shell completions.
 pub fn generate(mode: GenerateMode) -> anyhow::Result<()> {
+    let cmd = Cli::command();
+
     match mode {
         GenerateMode::Man => {
             clap_mangen::Man::new(Cli::command()).render(&mut io::stdout())?;
-            Ok(())
         }
-        _ => todo!()
+        GenerateMode::Bash => {
+            complete(Shell::Bash, cmd);
+        }
+        GenerateMode::Zsh => {
+            complete(Shell::Zsh, cmd);
+        }
+        GenerateMode::Fish => {
+            complete(Shell::Fish, cmd);
+        }
+        GenerateMode::Elvish => {
+            complete(Shell::Elvish, cmd);
+        }
+        GenerateMode::PowerShell => {
+            complete(Shell::PowerShell, cmd);
+        }
     }
+
+    Ok(())
 }
